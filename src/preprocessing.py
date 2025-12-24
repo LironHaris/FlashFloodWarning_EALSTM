56a6fecbadc20708dd4395e7de2e4cbfbe0a8677
Liron Haris <liron.haris@gmail.com>
2025-12-24 18:01:20 +0200

    Add preprocessing module

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,7 +5,6 @@ import config
 import logging
 from tqdm import tqdm
 
-# Configure logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
